ae1fd237eb03096d14e4e7fc4cfbf4c762bfd3df
Jeremiah-Jefry <g.jeremiahjefry06@gmail.com>
2026-01-27 23:35:26 +0530

    Add styles and templates for the Flask application

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def hello():
     return 'Hello, World!'
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
 
 @app.route('/home')
 def home():
